7ca9507b69af088cab50e45d22dcf00622b778ca
Andrew Selvarajah <andrew.sevarajah@uoit.net>
2020-10-13 21:20:42 -0400

    Updates

---
+++ precache-manifest.07ea913dfa48ca32ac029320666fc8e5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e338973be7cdcf3020d509083010aca7",
+    "url": "/first-website/index.html"
+  },
+  {
+    "revision": "42656b507ce3ee870aed",
+    "url": "/first-website/static/css/2.8ce86be7.chunk.css"
+  },
+  {
+    "revision": "484b360054c7d65da54a",
+    "url": "/first-website/static/css/main.c26193a1.chunk.css"
+  },
+  {
+    "revision": "42656b507ce3ee870aed",
+    "url": "/first-website/static/js/2.cda7d5ef.chunk.js"
+  },
+  {
+    "revision": "c83eaf460a8f55ef068d9679da1b7145",
+    "url": "/first-website/static/js/2.cda7d5ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "484b360054c7d65da54a",
+    "url": "/first-website/static/js/main.6de6bb74.chunk.js"
+  },
+  {
+    "revision": "4acd33789c20683537ac",
+    "url": "/first-website/static/js/runtime-main.c7c47633.js"
+  },
+  {
+    "revision": "1d262af8d6447aa6cf2cd710f96fbf62",
+    "url": "/first-website/static/media/dog.1d262af8.jpeg"
+  },
+  {
+    "revision": "19f13c26c95de4384d3ea2a44f051b1c",
+    "url": "/first-website/static/media/download.19f13c26.jpg"
+  },
+  {
+    "revision": "d533857f90d34931efe29c79ec176586",
+    "url": "/first-website/static/media/game.d533857f.jpg"
+  },
+  {
+    "revision": "41d9c660f6286d1938f8870a4fdeeba4",
+    "url": "/first-website/static/media/me.41d9c660.jpg"
+  },
+  {
+    "revision": "aabbaccd18aebac08d46404b482d4594",
+    "url": "/first-website/static/media/pool.aabbaccd.jpg"
+  }
+]);
